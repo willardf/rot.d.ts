6afd94adbc4214e0f9f94f41631cf67f71509169
Forest Willard <willardf@onid.orst.edu>
2014-09-27 15:04:05 -0700

    Initial commit. Display and String extensions.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 rot.d.ts
 ========
 
-rot.d.ts is the interface definitions of rot.js for Typescript.
+rot.d.ts is the interface definitions of rot.js for TypeScript.
+========
+
+## HOW TO USE
+
+Include rot.js in your HTML header via script tag and compile rot.d.ts with the rest of your TypeScript source. Then just be sure rot.js is included with your website.
+
+From there, everything is within the ROT namespace like normal!
---
+++ rot.d.ts
@@ -0,0 +1,45 @@
+﻿interface ROTStatic {
+    Display: Display;
+}
+
+// String extensions
+interface FormatFunc {
+    (format: string, ...args: any[]): string;
+    map: Object;
+}
+
+interface String {
+    format: FormatFunc;
+}
+
+// Display
+interface DisplayOptions {
+    width?: number;
+    height?: number;
+    fontSize?: number;
+    fontFamily?: string;
+    fontStyle?: string;
+    fg?: string;
+    bg?: string;
+    spacing?: number;
+    border?: number;
+    layout?: string;
+    tileWidth?: number;
+    tileHeight?: number;
+    tileMap?: Object;
+    tileSet?: Object;
+}
+
+interface Display{
+    new (options?: DisplayOptions): Display;
+    getContainer(): Node;
+    setOptions(options: DisplayOptions): void;
+    draw(x: number, y: number, character: any, fg: string, bg: string): void;
+    drawText(x: number, y: number, text: string, width: number): void;
+
+}
+
+// Helpers (Not in rot.js)
+declare enum FontStyle { Bold, Italic, None, Both }
+
+declare var ROT: ROTStatic;
