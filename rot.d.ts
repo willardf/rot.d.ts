0adc9d4193deedfa27fa2c85960bbc8469870eb3
Forest Willard <willardf@onid.orst.edu>
2014-09-27 16:49:31 -0700

    Using module to improve namespace pollution. Still a little wonky though...

--- rot.d.ts
+++ rot.d.ts
@@ -1,10 +1,4 @@
-﻿interface ROTStatic {
-    RNG: RNG;
-    Map: MapStatic;
-    Display: Display;
-}
-
-// String extensions
+﻿// String extensions
 interface FormatFunc {
     (...args: any[]): string;
     map: Object;
@@ -14,115 +8,161 @@ interface String {
     format: FormatFunc;
 }
 
-// Random Number Gen
-interface RNG {
-    getUniform(): number
-    getNormal(mean, stddev): number;
-    getPercentage(): number;
+declare module ROT {
+    var RNG: ROT.RNG;
+    var Map: ROT.MapStatic;
+    var Display: ROT.Display;
+    var Scheduler: ROT.SchedulerStatic;
 
-    getState(): Array<number>;
-    setState(state: Array<number>): void;
+    // Random Number Gen
+    interface RNG {
+        getUniform(): number;
+        getNormal(mean, stddev): number;
+        getPercentage(): number;
 
-    getSeed(): number;
-    setSeed(seed: number): void;
+        getState(): Array<number>;
+        setState(state: Array<number>): void;
+
+        getSeed(): number;
+        setSeed(seed: number): void;
+    }
+
+    // Map components
+    interface MapStatic {
+        Digger: Digger;
+        Uniform: Uniform;
+        Rogue: Rogue;
+    }
+
+    interface Map {
+        create(callback?: DigCallback): Digger;
+    }
+
+    // Map callbacks
+    interface DigCallback { (x: number, y: number, cellValue: number): void; }
+    interface DoorCallback { (x: number, y: number): void; }
+    interface PriorityWallCallback { (x: number, y: number): void; }
+    interface IsWallCallback { (x: number, y: number): boolean; }
+    interface CanBeDugCallback { (x: number, y: number): boolean; }
+
+    // Dungeon Generators
+    interface Dungeon extends Map {
+        getRooms(): Array<Room>;
+        getCorridors(): Array<Corridor>
+    }
+    /// <Summary>It's a Room</Summary>
+    interface Room {
+        getCenter(): number;
+        getLeft(): number;
+        getTop(): number;
+        getBottom(): number;
+        getRight(): number;
+
+        addDoor(x: number, y: number): Room;
+        addDoors(callback: IsWallCallback): Room;
+        getDoors(callback: DoorCallback): Room;
+        clearDoors(): Room;
+
+    }
+
+    interface Corridor {
+        new (startX: number, startY: number, endX: number, endY: number);
+        create(callback: DigCallback): boolean;
+        createPriorityWalls(callback: PriorityWallCallback): void;
+        isValid(isWallCallback: IsWallCallback, canBeDugCallback: CanBeDugCallback): boolean;
+    }
+
+    interface DiggerOptions {
+        roomWidth?: Array<number>;
+        roomHeight?: Array<number>;
+        corridorLength?: Array<number>;
+        dugPercentage?: number;
+        timeLimit?: number;
+    }
+
+    interface Digger extends Dungeon {
+        new (width?: number, height?: number, options?: DiggerOptions): Digger;
+    }
+
+    interface UniformOptions {
+        roomWidth?: Array<number>;
+        roomHeight?: Array<number>;
+        dugPercentage?: number;
+        timeLimit?: number;
+    }
+
+    interface Uniform extends Dungeon {
+        new (width?: number, height?: number, options?: UniformOptions): Uniform;
+    }
+
+    interface RogueOptions {
+        cellWidth?: Array<number>;
+        cellHeight?: Array<number>;
+        roomWidth?: Array<number>;
+        roomHeight?: Array<number>;
+    }
+
+    interface Rogue extends Map {
+        new (width?: number, height?: number, options?: RogueOptions): Rogue;
+    }
+
+    // Display
+    interface DisplayOptions {
+        width?: number;
+        height?: number;
+        fontSize?: number;
+        fontFamily?: string;
+        fontStyle?: string;
+        fg?: string;
+        bg?: string;
+        spacing?: number;
+        border?: number;
+        layout?: string;
+        tileWidth?: number;
+        tileHeight?: number;
+        tileMap?: Object;
+        tileSet?: Object;
+    }
+
+    interface Display {
+        DEBUG: DigCallback;
+
+        new (options?: DisplayOptions): Display;
+        getContainer(): Node;
+        setOptions(options: DisplayOptions): void;
+        draw(x: number, y: number, character: any, fg: string, bg: string): void;
+        drawText(x: number, y: number, text: string, width: number): void;
+
+    }
+
+    // Scheduler
+    interface SchedulerStatic {
+        Action: Action;
+        Simple: Simple;
+        Speed: Speed;
+    }
+
+    interface Scheduler {
+        add(item: any, repeat: boolean): Scheduler;
+        remove(item: any): boolean;
+        clear(): Scheduler;
+
+        next(): any;
+
+        getTime(): number;
+    }
+
+    interface Action extends Scheduler {
+        add(item: any, repeat: boolean, time?: number): Scheduler;
+    }
+
+    interface Simple extends Scheduler { }
+
+    interface SpeedActor { getSpeed(): number }
+    interface Speed extends Scheduler {
+        add(item: SpeedActor, repeat: boolean, time?: number): Scheduler;
+    }
+
+    // Helpers (Not in rot.js)
+    enum FontStyle { Bold, Italic, None, Both }
 }
-
-// Map components
-interface MapStatic {
-    Digger: Digger;
-    Uniform: Uniform;
-    Rogue: Rogue;
-}
-
-interface MapCreateCallback {
-    (x: number, y: number, cellValue: number):void;
-}
-
-interface DoorCallback { (x: number, y: number): void; }
-interface IsWallCallback { (x: number, y: number): boolean; }
-
-interface Room {
-    getCenter(): number;
-    getLeft(): number;
-    getTop(): number;
-    getBottom(): number;
-    getRight(): number;
-
-    addDoor(x: number, y: number): Room;
-    addDoors(callback: IsWallCallback): Room;
-    getDoors(callback: DoorCallback): Room;
-    clearDoors(): Room;
-
-}
-
-// Dungeon Generators
-interface DiggerOptions {
-    roomWidth?: Array<number>;
-    roomHeight?: Array<number>;
-    corridorLength?: Array<number>;
-    dugPercentage?: number;
-    timeLimit?: number;
-}
-
-interface Digger {
-    new (width?: number, height?: number, options?: DiggerOptions): Digger;
-
-    create(callback?:MapCreateCallback): Digger;
-
-    getRooms(): Array<Room>;
-}
-
-interface UniformOptions {
-    roomWidth?: Array<number>;
-    roomHeight?: Array<number>;
-    dugPercentage?: number;
-    timeLimit?: number;
-}
-
-interface Uniform {
-    new (width?: number, height?: number, options?: UniformOptions): Uniform;
-
-    create(callback?: MapCreateCallback): Uniform;
-
-    getRooms(): Array<Room>;
-}
-
-interface Rogue {
-    new (width?: number, height?: number): Rogue;
-
-    create(callback?: MapCreateCallback): Rogue;
-}
-
-// Display
-interface DisplayOptions {
-    width?: number;
-    height?: number;
-    fontSize?: number;
-    fontFamily?: string;
-    fontStyle?: string;
-    fg?: string;
-    bg?: string;
-    spacing?: number;
-    border?: number;
-    layout?: string;
-    tileWidth?: number;
-    tileHeight?: number;
-    tileMap?: Object;
-    tileSet?: Object;
-}
-
-interface Display{
-    DEBUG: MapCreateCallback;
-
-    new (options?: DisplayOptions): Display;
-    getContainer(): Node;
-    setOptions(options: DisplayOptions): void;
-    draw(x: number, y: number, character: any, fg: string, bg: string): void;
-    drawText(x: number, y: number, text: string, width: number): void;
-
-}
-
-// Helpers (Not in rot.js)
-declare enum FontStyle { Bold, Italic, None, Both }
-
-declare var ROT: ROTStatic;
